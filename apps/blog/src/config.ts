de7daee93559e7a2bca96331b49bad9224727e2d
Mogeko <zhengjunyi@live.com>
2024-02-13 11:27:17 +0100

    feat: update site configuration and meta tags

--- apps/blog/src/config.ts
+++ apps/blog/src/config.ts
@@ -1,24 +1,8 @@
-export const siteConfig: SiteConfig = {
-  title: "Mogeko's Blog",
-  author: {
-    name: "Mogeko",
-    url: "https://github.com/mogeko",
-    description: "A web developer",
-  },
-  footer: {
-    license: {
-      name: "CC BY-NC-SA 4.0",
-      url: "https://creativecommons.org/licenses/by-nc/4.0",
-    },
-    copyrightYear: `2017 - ${new Date().getFullYear()}`,
-  },
-};
+export const siteConfig = {
+  title: import.meta.env.SITE_BASE_TITLE || "Mogeko's Blog",
+  description: import.meta.env.SITE_DESCRIPTION || "A place to share my life.",
+  author: import.meta.env.SITE_AUTHOR || "Mogeko",
+  keywords: import.meta.env.SITE_EKY_WORD || ["blog", "programming"].join(", "),
+} as const;
 
-type SiteConfig = {
-  title: string;
-  author: { name: string; url: string; description?: string; bio?: string };
-  footer?: {
-    license: { name: string; url: string };
-    copyrightYear: string | number;
-  };
-};
+export type SiteConfig = typeof siteConfig;
--- apps/blog/src/env.d.ts
+++ apps/blog/src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SITE_BASE_TITLE?: string;
+  readonly SITE_DESCRIPTION?: string;
+  readonly SITE_AUTHOR?: string;
+  readonly SITE_EKY_WORD?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
