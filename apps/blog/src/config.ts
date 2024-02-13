1e04af45003a0b1ab2653d5e57a4b89f4945a62c
Mogeko <zhengjunyi@live.com>
2024-02-13 12:11:46 +0100

    feat: update site description and keywords in config.ts

--- apps/blog/src/config.ts
+++ apps/blog/src/config.ts
@@ -1,8 +1,11 @@
 export const siteConfig = {
   title: import.meta.env.SITE_BASE_TITLE || "Mogeko's Blog",
-  description: import.meta.env.SITE_DESCRIPTION || "A place to share my life.",
+  description:
+    import.meta.env.SITE_DESCRIPTION ||
+    "A place to share what I've learned, what I'm working on, and what I'm thinking about.",
   author: import.meta.env.SITE_AUTHOR || "Mogeko",
-  keywords: import.meta.env.SITE_KEYWORDS || ["blog", "programming"].join(", "),
+  keywords:
+    import.meta.env.SITE_KEYWORDS || "blog, programming, open source, software",
 } as const;
 
 export type SiteConfig = typeof siteConfig;
