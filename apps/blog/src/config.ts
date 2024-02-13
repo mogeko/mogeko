98a093c960cbb4535a13a455212fe4a5a6527e6c
Mogeko <zhengjunyi@live.com>
2024-02-13 11:42:43 +0100

    build: set relying on env for turborepo

--- apps/blog/src/config.ts
+++ apps/blog/src/config.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   title: import.meta.env.SITE_BASE_TITLE || "Mogeko's Blog",
   description: import.meta.env.SITE_DESCRIPTION || "A place to share my life.",
   author: import.meta.env.SITE_AUTHOR || "Mogeko",
-  keywords: import.meta.env.SITE_EKY_WORD || ["blog", "programming"].join(", "),
+  keywords: import.meta.env.SITE_EKYWORDS || ["blog", "programming"].join(", "),
 } as const;
 
 export type SiteConfig = typeof siteConfig;
