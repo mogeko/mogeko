3b3c524f787728985fd637f5ef4497aed63061c0
Mogeko <zhengjunyi@live.com>
2024-02-13 11:55:02 +0100

    fix: fix typo in SITE_KEYWORDS variable name

--- apps/blog/src/env.d.ts
+++ apps/blog/src/env.d.ts
@@ -5,7 +5,7 @@ interface ImportMetaEnv {
   readonly SITE_BASE_TITLE?: string;
   readonly SITE_DESCRIPTION?: string;
   readonly SITE_AUTHOR?: string;
-  readonly SITE_EKY_WORD?: string;
+  readonly SITE_KEYWORDS?: string;
 }
 
 interface ImportMeta {
