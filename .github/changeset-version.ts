1cbc864b14de122e761437cfe8f07be95982c4bb
Mogeko <zhengjunyi@live.com>
2024-02-14 05:14:29 +0100

    fix: ensure that the commands are executed in order

--- .github/changeset-version.ts
+++ .github/changeset-version.ts
@@ -7,18 +7,18 @@ import util from "node:util";
 const execPromise = util.promisify(exec);
 
 try {
-  const results = await Promise.all([
+  const results = [
     // This script is used by the `release.yml` workflow to update the version of the packages being released.
     // The standard step is only to run `changeset version` but this does not update the pnpm-lock.yaml file.
     // So we also run `pnpm install`, which does this update.
     // This is a workaround until this is handled automatically by `changeset version`.
     // See https://github.com/changesets/changesets/issues/421.
-    execPromise("pnpm changeset version"),
+    await execPromise("pnpm changeset version"),
     // Run `pnpm install` to update the pnpm-lock.yaml file.
-    execPromise("pnpm install --lockfile-only"),
-  ]);
+    await execPromise("pnpm install --lockfile-only"),
+  ];
 
-  console.log(results.map(({ stdout }) => stdout).join("\n"));
+  console.log(results.map(({ stdout, stderr }) => stdout || stderr).join("\n"));
 } catch (error) {
   console.error(error.message);
   process.exit(1);
