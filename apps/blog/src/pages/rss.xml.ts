1a9f5c2c4464213754e74525b40abb7ad55a64f8
Mogeko <zhengjunyi@live.com>
2024-02-15 23:53:18 +0100

    fix: it is not necessary to include content in rss

--- apps/blog/src/pages/rss.xml.ts
+++ apps/blog/src/pages/rss.xml.ts
@@ -1,29 +1,23 @@
-import emoji from "remark-emoji";
 import rss from "@astrojs/rss";
 import { siteConfig } from "@/config";
-import { createMarkdownProcessor } from "@astrojs/markdown-remark";
 import { getCollection } from "astro:content";
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
   const { author, title, description } = siteConfig;
-  const processor = await createMarkdownProcessor({ remarkPlugins: [emoji] });
   const blogs = await getCollection("posts");
 
   return rss({
     title: title,
     description: description,
     site: context.site ?? import.meta.env.SITE,
-    items: await Promise.all(
-      blogs.map(async ({ data, slug, body }) => ({
-        title: data.title,
-        description: data.description,
-        author: data.author ?? author,
-        content: (await processor.render(body)).code,
-        pubDate: data.date,
-        link: `/posts/${slug}`,
-      })),
-    ),
+    items: blogs.map(({ data, slug }) => ({
+      title: data.title,
+      description: data.description,
+      author: data.author ?? author,
+      pubDate: data.date,
+      link: `/posts/${slug}`,
+    })),
     stylesheet: "/rss/pretty-feed-v3.xsl",
   });
 }
