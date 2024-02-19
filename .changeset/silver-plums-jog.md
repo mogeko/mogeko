4092d9836b24c4a77b4e4b23eafd0a7a86a1eebd
Mogeko <zhengjunyi@live.com>
2024-02-19 22:01:35 +0100

    refactor: Refactor `assocPath` function

---
+++ .changeset/silver-plums-jog.md
@@ -0,0 +1,5 @@
+---
+"astro-reading-time": patch
+---
+
+Refactor `assocPath` function and add tests.
--- packages/astro-reading-time/tests/assoc-path.test.ts
+++ packages/astro-reading-time/tests/assoc-path.test.ts
@@ -29,4 +29,19 @@ describe("assocPath", () => {
 
     expect(result).toEqual(2);
   });
+
+  it("if a path incloud a number", () => {
+    const obj = { a: [1, { b: 2 }, 3] };
+    const result = assocPath(["a", 1, "b"], 4, obj);
+
+    expect(result).toEqual({ a: [1, { b: 4 }, 3] });
+  });
+
+  // It is not currect, but it is never hanppen in this project.
+  it("if a path ends with a number", () => {
+    const obj = { a: { b: 1 } };
+    const result = assocPath(["a", "b", 1], 2, obj);
+
+    expect(result).toEqual({ a: { b: { "1": 2 } } });
+  });
 });
