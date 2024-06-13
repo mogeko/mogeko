eddade27f3aa049225ccac6f60242cd63b29806a
Mogeko <zhengjunyi@live.com>
2024-06-14 01:25:36 +0800

    refactor: update font-family in globals.css and tailwind.config.ts
    Update the font-family in the globals.css and tailwind.config.ts files to use the variable --font-family-mono. This ensures consistent styling and improves code maintainability.

--- apps/blog/tailwind.config.ts
+++ apps/blog/tailwind.config.ts
@@ -65,6 +65,9 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        mono: "var(--font-family-mono)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
