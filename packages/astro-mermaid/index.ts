de42d1b44b9226d649f38bf5425336eb4690b4a4
Mogeko <zhengjunyi@live.com>
2024-06-27 12:33:40 +0800

    feat: add `rehype-mermaid` to render SVG

--- packages/astro-mermaid/index.ts
+++ packages/astro-mermaid/index.ts
@@ -1,10 +1,22 @@
 import { addMermaidClass } from "@/src/shiki-transformer";
+import rehypeMermaid from "rehype-mermaid";
 import type { AstroIntegration } from "astro";
 
 export default function mermaid(): AstroIntegration {
   return {
     name: "astro-mermaid",
-    hooks: {},
+    hooks: {
+      "astro:config:setup": ({ updateConfig }) => {
+        updateConfig({
+          markdown: {
+            shikiConfig: {
+              transformers: [addMermaidClass()],
+            },
+            rehypePlugins: [rehypeMermaid],
+          },
+        });
+      },
+    },
   };
 }
 
