3ff29a381b5c2c4050ba5b8cacdbef7a41ec02a2
Mogeko <zhengjunyi@live.com>
2024-06-29 01:14:20 +0800

    feat: improve the type signature of the function `isNil`

--- packages/utils/src/is-nil.ts
+++ packages/utils/src/is-nil.ts
@@ -1,3 +1,7 @@
-export function isNil(value: unknown) {
+// TypeScript does not have a `NaN` type yet.
+// See: https://github.com/Microsoft/TypeScript/issues/28682
+export function isNil<T>(
+  value: unknown,
+): value is (null | undefined | typeof NaN) & T {
   return value == null || Number.isNaN(value);
 }
