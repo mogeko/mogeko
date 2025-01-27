2c2c11c42d279a4c0de94db033006f043698e53d
Mogeko <zhengjunyi@live.com>
2025-01-27 15:18:03 +0800

    build: setup Content Collections

--- apps/blog/next.config.ts
+++ apps/blog/next.config.ts
@@ -1,7 +1,12 @@
+import { createContentCollectionPlugin } from "@content-collections/next";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withContent = createContentCollectionPlugin({
+  configPath: "./content/config.ts",
+});
+
+export default withContent(nextConfig);
