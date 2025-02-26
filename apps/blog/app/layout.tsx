f67f006f4403362e651fee5577792c2b664066bb
Mogeko <zhengjunyi@live.com>
2025-02-26 21:33:58 +0800

    chore: update project configuration and dependencies

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+import "@/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
