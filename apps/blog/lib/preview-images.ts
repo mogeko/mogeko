b8a993a5b635612c898d08173f23ba63e9f655fe
Mogeko <zhengjunyi@live.com>
2025-03-11 01:38:03 +0800

    feat: ensure that erroneous results are not cached.

--- apps/blog/lib/preview-images.ts
+++ apps/blog/lib/preview-images.ts
@@ -1,27 +1,17 @@
 import lqip, { type LqipModernOutput } from "lqip-modern";
 import { unstable_cache } from "next/cache";
 
+type Metadate = LqipModernOutput["metadata"];
+
 export async function getPreviewImage(url: URL, retry = 0) {
   if (retry >= 3) return null;
 
   const createPreviewImage = unstable_cache(
-    async (): Promise<LqipModernOutput["metadata"] | null> => {
-      try {
-        const body = await (await fetch(url)).arrayBuffer();
-        const { metadata } = await lqip(body);
+    async (): Promise<Metadate> => {
+      const body = await (await fetch(url)).arrayBuffer();
+      const { metadata } = await lqip(body);
 
-        return metadata;
-      } catch (err: any) {
-        console.warn(
-          `Failed to create preview image (retry: ${retry})`,
-          url,
-          err.message,
-        );
-
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-
-        return getPreviewImage(url, retry + 1);
-      }
+      return metadata;
     },
     [url.pathname],
     {
@@ -30,5 +20,19 @@ export async function getPreviewImage(url: URL, retry = 0) {
     },
   );
 
-  return createPreviewImage();
+  try {
+    return await createPreviewImage();
+  } catch (err: any) {
+    console.warn(
+      `Failed to create preview image (retry: ${retry})`,
+      url,
+      err.message,
+    );
+
+    return await new Promise<Metadate | null>((resolve) => {
+      setTimeout(() => {
+        resolve(getPreviewImage(url, retry + 1));
+      }, 3000);
+    });
+  }
 }
