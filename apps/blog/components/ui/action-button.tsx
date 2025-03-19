f5dd924fde891547a332cd28dbbd0bc6461bbbf2
Mogeko <zhengjunyi@live.com>
2025-03-19 20:45:03 +0800

    feat: enhance ActionButton component with asChild prop and integrate Slot for improved flexibility

--- apps/blog/components/ui/action-button.tsx
+++ apps/blog/components/ui/action-button.tsx
@@ -1,13 +1,19 @@
 import { cn } from "@/lib/utils";
-import { Root } from "@radix-ui/react-toggle";
+import { Slot } from "@radix-ui/react-slot";
 import React from "react";
 
 export const ActionButton = React.forwardRef<
-  React.ComponentRef<typeof Root>,
-  React.ComponentPropsWithoutRef<typeof Root> & { hotkey?: React.ReactNode }
->(({ className, children, hotkey, ...props }, ref) => {
+  HTMLButtonElement,
+  React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    hotkey?: React.ReactNode;
+    open?: boolean;
+    asChild?: boolean;
+  }
+>(({ className, children, hotkey, open, asChild, ...props }, ref) => {
+  const Comp = asChild ? Slot : "button";
+
   return (
-    <Root
+    <Comp
       className={cn(
         "group inline-flex items-center justify-center cursor-pointer outline-0 shrink-0",
         className,
@@ -16,14 +22,19 @@ export const ActionButton = React.forwardRef<
       {...props}
     >
       {hotkey && (
-        <span className="bg-muted text-muted-foreground font-normal px-[1ch] py-0 select-none group-has-hover:bg-accent group-has-hover:text-accent-foreground group-has-focus:bg-accent group-has-focus:text-accent-foreground group-data-[state=on]:bg-accent group-data-[state=on]:text-accent-foreground">
+        <kbd className="bg-muted text-muted-foreground font-normal px-[1ch] py-0 select-none group-has-hover:bg-accent group-has-hover:text-accent-foreground group-has-focus:bg-accent group-has-focus:text-accent-foreground">
           {hotkey}
-        </span>
+        </kbd>
       )}
-      <span className="bg-secondary text-secondary-foreground shadow-muted shadow-[inset_0_0_0_2px] font-normal px-[1ch] py-0 uppercase select-none group-has-hover:shadow-accent group-has-focus:shadow-accent group-data-[state=on]:shadow-accent">
+      <span
+        className={cn(
+          "bg-secondary text-secondary-foreground shadow-muted shadow-[inset_0_0_0_2px] font-normal px-[1ch] py-0 uppercase select-none group-has-hover:shadow-accent group-has-focus:shadow-accent",
+          open && "bg-accent",
+        )}
+      >
         {children}
       </span>
-    </Root>
+    </Comp>
   );
 });
 ActionButton.displayName = "ActionButton";
