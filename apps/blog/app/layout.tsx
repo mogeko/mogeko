34a3820d9ec4a87695ddb5ac70014d64ff26ac24
Mogeko <zhengjunyi@live.com>
2025-03-30 02:15:55 +0800

    refactor: update favicon and improve loading animation

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -41,12 +41,6 @@ const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
           rel="stylesheet"
           href="https://static.zeoseven.com/zsft/442/main/result.css"
         />
-        <link rel="icon" href="/favicon.ico" sizes="32x32" />
-        <link rel="icon" href="/icon/svg" type="image/svg+xml" sizes="any" />
-        <link rel="icon" href="/icon/192" type="image/png" sizes="192x192" />
-        <link rel="icon" href="/icon/512" type="image/png" sizes="512x512" />
-        <link rel="apple-touch-icon" href="/apple-icon?<generated>" />
-        <link rel="manifest" href="/manifest.webmanifest" />
         <style>{`
           :root {
             --font-mono: "Maple Mono NF CN", ui-monospace, monospace;
