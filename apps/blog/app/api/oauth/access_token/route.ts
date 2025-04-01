c20264e16c5b5e7f394aabd62d92aebb842c274d
Mogeko <zhengjunyi@live.com>
2025-04-02 01:32:24 +0800

    feat: implement OAuth access token and authorization routes with AES-GCM encryption

---
+++ apps/blog/app/api/oauth/access_token/route.ts
@@ -0,0 +1,74 @@
+import { decrypt, encrypt } from "@/lib/ase-gcm";
+import { type NextRequest, NextResponse } from "next/server";
+
+const TOKEN_VALIDITY_PERIOD = 1000 * 60 * 60 * 24 * 365; // 1 year;
+
+export async function GET({ nextUrl }: NextRequest) {
+  const state = nextUrl.searchParams.get("state");
+  const code = nextUrl.searchParams.get("code");
+
+  if (!state || !code) {
+    return NextResponse.json(
+      { error: "`state` and `code` are required." },
+      { status: 400 },
+    );
+  }
+
+  const client_id = process.env.OAUTH_CLIENT_ID;
+  const client_secret = process.env.OAUTH_CLIENT_SECRET;
+  const passwd = process.env.ENCRYPTION_PASSWD;
+
+  if (!client_id || !client_secret || !passwd) {
+    return NextResponse.json(
+      { error: "Internal server error." },
+      { status: 500 },
+    );
+  }
+
+  let callbackURL: string;
+  try {
+    callbackURL = JSON.parse(await decrypt(state, passwd)).value;
+  } catch (_) {
+    return NextResponse.json({ error: "Invalid `state`." }, { status: 400 });
+  }
+
+  const response = NextResponse.redirect(callbackURL);
+
+  try {
+    const resp = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      body: new URLSearchParams({
+        redirect_uri: `${nextUrl.origin}/api/oauth/access_token`,
+        client_id,
+        client_secret,
+        code,
+      }),
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (resp.ok) {
+      const oauthToken = await encrypt(
+        JSON.stringify({
+          value: (await resp.json()).access_token,
+          expires: Date.now() + TOKEN_VALIDITY_PERIOD,
+        }),
+        passwd,
+      );
+
+      response.cookies.set("oauth_token", oauthToken, {
+        path: "/", // Required for Safari
+        expires: new Date(Date.now() + TOKEN_VALIDITY_PERIOD),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      });
+    } else {
+      throw new Error(`Failed to get access token: ${resp.statusText}`);
+    }
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+
+  return response;
+}
