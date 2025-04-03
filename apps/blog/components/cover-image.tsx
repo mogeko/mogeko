0fa297b9f7a5e189d080db806127f9350ec98a16
Mogeko <zhengjunyi@live.com>
2025-04-03 21:38:51 +0800

    feat: add CoverImage component for rendering images from files property

---
+++ apps/blog/components/cover-image.tsx
@@ -0,0 +1,18 @@
+import { Image } from "@/components/ui/image";
+import type { FilesPropertyItemObjectResponse } from "@/lib/api-endpoints";
+
+export const CoverImage: React.FC<{
+  property: Pick<FilesPropertyItemObjectResponse, "files">;
+  width: number;
+  className?: string;
+}> = ({ property, width, className }) => {
+  return property.files.map((image) => {
+    if (image.type === "file") {
+      return (
+        <div className={className}>
+          <Image width={width} src={image.file.url} alt={image.name} />
+        </div>
+      );
+    }
+  });
+};
