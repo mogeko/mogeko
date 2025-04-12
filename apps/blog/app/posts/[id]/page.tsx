977f0db519fcc64774683a43c5634a43e9da4bb1
Mogeko <zhengjunyi@live.com>
2025-04-13 00:50:56 +0800

    refactor: simplify Author component by removing user_id prop

--- apps/blog/app/posts/[id]/page.tsx
+++ apps/blog/app/posts/[id]/page.tsx
@@ -75,7 +75,7 @@ const Page: NextPage<Props> = async ({ params }) => {
           // In order to optimize Cumulative Layout Shift (CLS)
           fallback={<Loading className="h-2" />}
         >
-          <Author page={page} user_id={page.created_by.id /* Long ID */} />
+          <Author page={page} />
         </Suspense>
       </section>
       <Separator className="mt-1 mb-3" />
