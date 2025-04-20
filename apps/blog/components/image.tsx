c7e20e8273dbda5789d0caf8e145b56a5ea9abe0
Mogeko <zhengjunyi@live.com>
2025-04-20 12:28:49 +0800

    refactor: rename imageLoader to cloudinaryLoader and update its type definition

--- apps/blog/components/image.tsx
+++ apps/blog/components/image.tsx
@@ -3,7 +3,7 @@ import { subtle } from "node:crypto";
 import { parse } from "node:path";
 import { URL } from "node:url";
 import { TextEncoder } from "node:util";
-import { imageLoader } from "@/lib/image-loader";
+import { cloudinaryLoader } from "@/lib/image-loader";
 import { v2 as cloudinary } from "cloudinary";
 import { unstable_cache as cache } from "next/cache";
 import NextImage from "next/image";
@@ -24,7 +24,7 @@ export const Image: React.FC<
   return (
     <NextImage
       height={props.width ? (height / width) * Number(props.width) : height}
-      loader={imageLoader}
+      loader={cloudinaryLoader}
       src={public_id}
       alt={alt.length ? alt : fileName}
       {...props}
