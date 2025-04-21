7e19f7c3660566919969c52225a4479f61a6fdc8
Mogeko <zhengjunyi@live.com>
2025-04-21 20:48:11 +0800

    feat: add checkbox component and update related dependency versions

---
+++ apps/blog/components/ui/checkbox.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+
+export const Checkbox: React.FC<
+  React.HTMLAttributes<HTMLDivElement> & { checked?: boolean }
+> = ({ checked = false, className, children, ...props }) => {
+  return (
+    <div
+      className={cn(
+        "inline-flex items-center justify-center cursor-pointer outline-none shrink-0",
+        className,
+      )}
+      {...props}
+    >
+      <span className="inline-flex w-full h-full px-[1ch] text-foreground bg-border items-stretch">
+        {checked ? "\u2573" : "\u00A0"}
+      </span>
+      <span className="inline-flex bg-secondary min-w-1/10 w-full items-stretch">
+        {children}
+      </span>
+    </div>
+  );
+};
