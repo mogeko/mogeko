283653107be3d06271197924c1ffc9c1cee74127
Zheng Junyi <zhengjunyi@live.com>
2025-10-17 01:51:43 +0800

    fix: update variable name for consistency in image loader

--- apps/blog/lib/image-loader.ts
+++ apps/blog/lib/image-loader.ts
@@ -3,11 +3,11 @@
 import type { ImageLoader } from "next/image";
 
 // Need to set `images.remotePatterns` in `next.config.js` to allow loading images from ImageKit
-const IK_BASE_URL = "https://ik.imagekit.io/mogeko";
+const IMAGEKIT_BASE_URL = "https://ik.imagekit.io/mogeko";
 
 // Docs: https://imagekit.io/docs/image-transformation
 export const imageKitLoader: ImageLoader = ({ src, width, quality }) => {
   const params = [`w-${width}`, `q-${quality || 80}`];
 
-  return `${IK_BASE_URL}/${src}?tr=${params.join(",")}`;
+  return `${IMAGEKIT_BASE_URL}${src}?tr=${params.join(",")}`;
 };
