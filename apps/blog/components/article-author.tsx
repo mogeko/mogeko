26afc078e29ac291adb832d0a365e32c169b76f3
Zheng Junyi <zhengjunyi@live.com>
2025-10-24 00:03:02 +0800

    feat: add spinner fallback for author avatar loading and remove unused cover image component

--- apps/blog/components/article-author.tsx
+++ apps/blog/components/article-author.tsx
@@ -1,5 +1,7 @@
 import { intlFormat } from "date-fns";
+import { Suspense } from "react";
 import { Image } from "@/components/image";
+import { Spinner } from "@/components/ui/spinner";
 import { notion } from "@/lib/notion";
 import { retrievePage } from "@/lib/retrieve-page";
 
@@ -16,16 +18,18 @@ export const Author: React.FC<{
 
     return (
       <div className="flex justify-start items-center">
-        {avatar_url && (
-          <Image
-            uploadId={user_id}
-            src={avatar_url}
-            className="w-[43px] mr-[calc(5ch-43px)] inline-block relative h-2"
-            height={43}
-            width={43}
-            alt={name ?? "Author"}
-          />
-        )}
+        <Suspense fallback={<Spinner className="w-[43px] h-[43px]" />}>
+          {avatar_url && (
+            <Image
+              uploadId={user_id}
+              src={avatar_url}
+              className="w-[43px] mr-[calc(5ch-43px)] inline-block relative h-2"
+              height={43}
+              width={43}
+              alt={name ?? "Author"}
+            />
+          )}
+        </Suspense>
         <div>
           <p>{name ?? "Anonymous"}</p>
           <p>{intlFormat(publidhDate ?? page.created_time)}</p>
