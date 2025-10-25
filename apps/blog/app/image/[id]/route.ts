620189a7b33fdb299adb9576c23e79dded4fab1d
Zheng Junyi <zhengjunyi@live.com>
2025-10-25 18:13:52 +0800

    fix: fix the type of the getImage function.

--- apps/blog/app/image/[id]/route.ts
+++ apps/blog/app/image/[id]/route.ts
@@ -1,13 +1,16 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { getImageParams } from "@/lib/image-upload";
+import type { NotionImageResp } from "@/components/image";
+import { getImage, type ImageResp } from "@/lib/image-helper";
 
 type RContext = RouteContext<"/image/[id]">;
 
 export async function GET(req: NextRequest, ctx: RContext) {
   const { id } = await ctx.params;
 
-  const redirectURL = await getImageParams(id).then((data) => {
-    if (data) {
+  type Resp = ImageResp | NotionImageResp;
+
+  const redirectURL = await getImage<Resp>(id).then((data) => {
+    if (data && "filePath" in data) {
       return new URL(`/image/${data.filePath}`, req.url);
     } else {
       return new URL("/404", req.url);
--- apps/blog/components/image.tsx
+++ apps/blog/components/image.tsx
@@ -1,7 +1,9 @@
 import { basename, parse } from "node:path/posix";
 import { URL } from "node:url";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import NextImage from "next/image";
-import { getImageParams, upload } from "@/lib/image-upload";
+import { getImage, type ImageResp, setImage } from "@/lib/image-helper";
+import { BUCKET_NAME, s3 } from "@/lib/s3";
 
 export const Image: React.FC<
   React.ComponentProps<typeof NextImage> & { uploadId?: string }
@@ -11,15 +13,17 @@ export const Image: React.FC<
   const key = uploadId || basename(dir);
 
   try {
-    const data = await getImageParams(key).then((data) => {
-      return data || upload({ key, url, fileName });
-    });
+    const data: NotionImageResp | ImageResp = await getImage(key).then(
+      (data) => {
+        return data || upload({ key, url, fileName });
+      },
+    );
 
-    const { height, width, filePath, name, blurDataURL, mimeType } = data;
+    const { height, width, name, blurDataURL, mimeType, ...rest } = data;
 
     return (
       <NextImage
-        src={`/image/${filePath}`}
+        src={"filePath" in rest ? `/image/${rest.filePath}` : src}
         height={props.width ? (height / width) * Number(props.width) : height}
         alt={alt.length ? alt : name}
         unoptimized={mimeType === "image/svg+xml"}
@@ -33,3 +37,30 @@ export const Image: React.FC<
     return <img src={url.toString()} alt={alt} />;
   }
 };
+
+export type NotionImageResp = ImageResp & { filePath: string };
+
+export async function upload(
+  options: Parameters<typeof setImage>[number],
+): Promise<NotionImageResp> {
+  const filePath = `${options.key}/${options.fileName}`;
+
+  return await setImage(async (buffer, meta) => {
+    const { ETag: _eTag } = await s3.send(
+      new PutObjectCommand({
+        Bucket: BUCKET_NAME,
+        Body: buffer,
+        Key: filePath,
+        ContentType: meta.mimeType,
+        Metadata: {
+          uploadedBy: "mogeko-blog",
+          height: meta.height.toString(),
+          width: meta.width.toString(),
+          blurDataURL: meta.blurDataURL,
+        },
+      }),
+    );
+
+    return { ...meta, filePath };
+  }, options);
+}
