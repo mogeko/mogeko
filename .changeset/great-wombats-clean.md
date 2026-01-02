3f41ca8d4e246c4043c7bcf958e946813e72bc3d
Zheng Junyi <zhengjunyi@live.com>
2026-01-02 14:27:10 +0800

    fix: add request and response checksum configuration

---
+++ .changeset/great-wombats-clean.md
@@ -0,0 +1,9 @@
+---
+"@mogeko/blog": patch
+---
+
+Set request and response checksum configuration for S3 client.
+
+In the v3.729.0 release, AWS introduced new compatibility changes.
+
+See: https://github.com/aws/aws-sdk-js-v3/issues/6810
--- apps/blog/lib/s3.ts
+++ apps/blog/lib/s3.ts
@@ -7,4 +7,8 @@ export const BUCKET_NAME = process.env.R2_BUCKET_NAME;
 export const s3 = new S3Client({
   region: "auto",
   endpoint: process.env.R2_ENDPOINT,
+  // In the v3.729.0 release, AWS introduced new compatibility changes.
+  // See: https://github.com/aws/aws-sdk-js-v3/issues/6810
+  requestChecksumCalculation: "WHEN_REQUIRED",
+  responseChecksumValidation: "WHEN_REQUIRED",
 });
